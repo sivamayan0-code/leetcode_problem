97b64210983650add3e41c32afd078b7db67aeb9
sivamayan <sivamayan0@gmail.com>
2026-04-15 18:46:40 +0530

    Add files via upload

---
+++ Maximum product.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = maxSoFar;
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            if (curr < 0) {
+                int temp = maxSoFar;
+                maxSoFar = minSoFar;
+                minSoFar = temp;
+            }
+            maxSoFar = Math.max(curr, maxSoFar * curr);
+            minSoFar = Math.min(curr, minSoFar * curr);
+
+            result = Math.max(result, maxSoFar);
+        }
+
+        return result;
+    }
+}
---
+++ power(x,n).java
@@ -0,0 +1,7 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double s = Math.pow(x,n);  
+        return s;      
+    }
+    
+}
